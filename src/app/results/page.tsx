56a58a92dad62a410f4be46b22384b4e22375700
aman <aman@101edu.co>
2025-01-18 15:45:53 +0530

    add layout

---
+++ src/app/results/page.tsx
@@ -0,0 +1,9 @@
+import ResultsDisplay from '../components/ResultsDisplay'
+
+export default function Results() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center p-4 md:p-8 bg-[#F5F8FB]">
+      <ResultsDisplay />
+    </main>
+  )
+}
